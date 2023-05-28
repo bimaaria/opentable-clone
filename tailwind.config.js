0164a4eac75ca95e56ac50acd08f329d697896d6
bimaaria <bima.aria.radjasa@gmail.com>
2023-05-28 07:24:23 +0700

    setup project

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,19 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    fontSize: {
+      "2xsm": "10px",
+      xsm: "12px",
+      sm: "14px",
+      reg: "16px",
+      lg: "18px",
+      xl: "20px",
+      "2xl": "24px",
+      "3xl": "28px",
+      "4xl": "32px",
+      "5xl": "40px",
+      "6xl": "44px",
+    }
   },
   plugins: [],
 }
