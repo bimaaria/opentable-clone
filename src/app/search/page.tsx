c2449371f18d9cf9eb302eab7429d84c14cfd1e8
bimaaria <bima.aria.radjasa@gmail.com>
2023-05-28 07:27:22 +0700

    create search page

---
+++ src/app/search/page.tsx
@@ -0,0 +1,20 @@
+import Header from "./components/Header"
+import RestaurantCard from "./components/RestaurantCard"
+import Sidebar from "./components/Sidebar"
+
+const Search = () => {
+  return (
+    <>
+      <Header />
+      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+        <Sidebar />
+        <div className="w-5/6">
+          <RestaurantCard />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Search
+
