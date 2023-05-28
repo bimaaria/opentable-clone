af70b4ed9f90f37da9fd4123422b05ded322bc94
bimaaria <bima.aria.radjasa@gmail.com>
2023-05-28 07:26:59 +0700

    create reservation page

---
+++ src/app/reservation/[slug]/page.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import ReservationForm from "./components/ReservationForm"
+
+const Reservation = () => {
+  return (
+    <div className="border-t h-screen">
+      <div className="py-9 w-3/5 m-auto"> 
+        <Header />
+        <ReservationForm />
+      </div>
+    </div>
+  )
+}
+
+export default Reservation
