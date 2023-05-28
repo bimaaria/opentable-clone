566b09f16f7aa12266439328fa5cca6f254f8dcd
bimaaria <bima.aria.radjasa@gmail.com>
2023-05-28 12:15:50 +0700

    implement search feature

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -8,12 +8,14 @@ const SearchBar = () => {
   const [location, setLocation] = React.useState("");
 
   const handleSearch = () => {
-    if(location === "banana") return;
-    router.push("/search");
+    if(location === "") return;
+    router.push(`/search?city=${location}`);
+    setLocation("");
+    console.log(location)
   }
 
   return (
-    <div className="text-left text-lg py-3 m-auto flex justify-center">
+    <div className="flex justify-center py-3 m-auto text-lg text-left">
       <input
         className="rounded  mr-3 p-2 w-[450px]"
         type="text"
@@ -21,7 +23,7 @@ const SearchBar = () => {
         value={location}
         onChange={(e) => setLocation(e.target.value)}
       />
-      <button className="rounded bg-red-600 px-9 py-2 text-white" onClick={handleSearch}>
+      <button className="py-2 text-white bg-red-600 rounded px-9" onClick={handleSearch}>
         Let's go
       </button>
     </div>
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,15 +1,53 @@
+import { PrismaClient } from "@prisma/client"
 import Header from "./components/Header"
 import RestaurantCard from "./components/RestaurantCard"
 import Sidebar from "./components/Sidebar"
 
-const Search = () => {
+const prisma = new PrismaClient();
+
+const fetchRestaurantByCity = async (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true
+  }
+
+  if(!city) return await prisma.restaurant.findMany({select});
+  
+  const restaurant = await prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLowerCase()
+        }
+      }
+    },
+    select
+  });
+
+  return restaurant;
+} 
+
+const Search = async ({ searchParams }: { searchParams: { city: string }}) => {
+  const restaurants = await fetchRestaurantByCity(searchParams.city);
+  
   return (
     <>
       <Header />
-      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+      <div className="flex items-start justify-between w-2/3 py-4 m-auto">
         <Sidebar />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length ? (
+            restaurants.map(restaurant => (
+              <RestaurantCard restaurant={restaurant} /> 
+            ))
+          ) : (
+            <p>Sorry, we found no restaurant in this area</p>
+          )}
         </div>
       </div>
     </>
