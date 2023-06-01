dee4fc2359dc0fdad361746cc93f373ed3aedb07
bimaaria <bima.aria.radjasa@gmail.com>
2023-06-01 07:50:34 +0700

    create unit test

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,10 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import NavBar from '@/app/components/NavBar';
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { AppRouterContext } from "next/dist/shared/lib/app-router-context";
+import userEvent from "@testing-library/user-event"
+import NavBar from "@/app/components/NavBar";
+import RestaurantCard from "@/app/search/components/RestaurantCard";
+import { RESTAURANT } from "../mock/data";
+import SearchBar from "@/app/components/SearchBar";
+import { createMockRouter } from "../mock/createMockRouter";
+import Price from "@/app/components/Price";
  
-describe('Navbar', () => {
-  
-  it('renders a link to home component', () => {
+describe("Home", () => {
+  it("renders a link to home component", () => {
     render(<NavBar />);
     
     const navbar = screen.getByRole("link");
@@ -31,5 +37,62 @@ describe('Navbar', () => {
     });
     
     expect(signUpButton).toBeInTheDocument();
+  });
+
+  it("renders the search bar", async () => {  
+    const user = userEvent.setup();
+    const router = createMockRouter({
+      query: "toronto"
+    })
+
+    render(
+      <AppRouterContext.Provider value={router}>
+        <SearchBar />
+      </AppRouterContext.Provider>
+    );
+
+    const searchInput = screen.getByPlaceholderText(/state, city or town/i);
+    
+    const searchButton = screen.getByRole("button", {
+      name: /let's go/i
+    });
+    
+    expect(searchInput).toBeInTheDocument();
+    
+    await user.type(searchInput, "toronto");
+    expect(searchInput.value).toBe("toronto");
+    
+    expect(searchButton).toBeInTheDocument();
+  });
+  
+  it("should navigate to search page", async () => {
+    const user = userEvent.setup();
+    const router = createMockRouter({
+      query: "toronto"
+    });
+
+    const searchButton = screen.getByRole("button", {
+      name: /let's go/i
+    });
+    
+    await user.click(searchButton);
+    expect(router.push).toHaveBeenCalledWith("/search?city=toronto");
+  });
+
+  it("renders the price", () => {
+    render(<Price price={RESTAURANT.price} />);
+
+    const price = screen.getByText("$$");
+
+    expect(price).toBeInTheDocument();
+  });
+  
+  it("render the restaurant card", () => {
+    render(<RestaurantCard restaurant={RESTAURANT} />);
+
+    const paragraph = screen.getByRole("link");
+  
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph).toHaveAttribute("href", "/restaurant/vivaan-fine-indian-cuisine-ottawa");
   });
 });
---
+++ mock/data.ts
@@ -0,0 +1,25 @@
+export const RESTAURANT = {
+  name: "Vivaan - fine Indian",
+  main_image:
+    "https://resizer.otstatic.com/v2/photos/wide-huge/1/32109459.jpg",
+  price: "REGULAR",
+  description:
+    "Vivaan is Modern Indian Cuisine serving dishes from different regions of India. We carefully select our ingredients and use them to make authentic Indian recipes and our chef puts his modern flair and twists to the dishes.",
+  images: [
+    "https://resizer.otstatic.com/v2/photos/xlarge/2/32109461.jpg",
+    "https://resizer.otstatic.com/v2/photos/xlarge/1/32459786.jpg",
+    "https://resizer.otstatic.com/v2/photos/xlarge/1/32484701.jpg",
+    "https://resizer.otstatic.com/v2/photos/xlarge/1/32484708.jpg",
+  ],
+  open_time: "14:30:00.000Z",
+  close_time: "21:30:00.000Z",
+  slug: "vivaan-fine-indian-cuisine-ottawa",
+  location: {
+    id: 1,
+    name: "Ottawa"
+  },
+  cuisine: {
+    id: 1,
+    name: "Indian"
+  },
+}
