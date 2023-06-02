20bcf1510caf3fc083cdff1c4154153c1b12a5c6
bimaaria <bima.aria.radjasa@gmail.com>
2023-06-02 15:56:36 +0700

    implement middleware

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import * as jose from "jose";
+
+export async function middleware(req: NextRequest, res: NextResponse) {
+  const bearerToken = req.headers.get("authorization") as string;
+
+  if(!bearerToken) {
+    return new NextResponse(
+      JSON.stringify({
+        errorMessage: "Unauthorized"
+      }), { status: 401 }
+    )
+  }
+
+  const token = bearerToken.split(" ")[1];
+
+  if(!token) {
+    return new NextResponse(
+      JSON.stringify({
+        errorMessage: "Unauthorized"
+      }), { status: 401 }
+    )
+  }
+
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+  try {
+    await jose.jwtVerify(token, secret);
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({
+        errorMessage: "Unauthorized"
+      }), { status: 401, headers: { 'content-type': 'application/json' }}
+    )
+  }
+}
+
+export const config = {
+  matcher: ["/api/auth/me"]
+}
